f297e6e180638b6f967e6454913240fb4a0dbd7f
Baljeet Singh <baljeet.tech01@gmail.com>
2022-02-23 22:47:27 +0530

    People of GOT

--- block-BJaadr/code/index.js
+++ block-BJaadr/code/index.js
@@ -2,35 +2,76 @@
 
 function countAllPeople() {
   // your code goes here
+  return got.houses.reduce((acc,cv) => {
+    acc = acc + cv.people.length;
+    return acc;
+  }, 0);
 }
 
 function peopleByHouses() {
   // your code goes here
+  return got.houses.reduce((acc,cv) => {
+    acc[cv.name] =  cv.people.length;
+    return acc;
+  }, {});
 }
 
 function everyone() {
   // your code goes here
+  return got.houses.reduce((acc,cv) => {
+    acc =   acc.concat(cv.people.map((n) => n.name));
+    return acc;
+  }, []);
 }
 
 function nameWithS() {
   // your code goes here
+  return got.houses.reduce((acc,cv) => {
+    acc =   acc.concat(cv.people.map((n) => n.name).filter((names) => names.toLowerCase().includes(`s`) ));
+    return acc;
+  }, []);
 }
+
 
 function nameWithA() {
   // your code goes here
+  return got.houses.reduce((acc,cv) => {
+    acc =   acc.concat(cv.people.map((n) => n.name).filter((names) => names.toLowerCase().includes(`a`) ));
+    return acc;
+  }, []);
 }
+
+
 
 function surnameWithS() {
   // your code goes here
+    // your code goes here
+    return got.houses.reduce((acc,cv) => {
+      acc =   acc.concat(cv.people.map((n) => n.name).filter((names) => names.split(" ")[1].toLowerCase().includes(`s`) ));
+      return acc;
+    }, []);  
 }
+
 
 function surnameWithA() {
   // your code goes here
+  return got.houses.reduce((acc,cv) => {
+    acc =   acc.concat(cv.people.map((n) => n.name).filter((names) => names.split(" ")[1].toLowerCase().includes(`a`) ));
+    return acc;
+  }, []);  
 }
+
+
 
 function peopleNameOfAllHouses() {
   // your code goes here
+  return got.houses.reduce((acc,cv) => {
+    acc[cv.name] =  cv.people.map((p) => p.name);
+    return acc;
+  }, {});
 }
+
+
 
 // Testing your result after writing your function
 console.log(countAllPeople());
@@ -63,3 +104,4 @@ console.log(surnameWithA());
 console.log(peopleNameOfAllHouses());
 // Output should be
 // {Arryns: ["Jon Arryn"], Baratheons: ["Robert Baratheon", "Stannis Baratheon", "Renly Baratheon", "Joffrey Baratheon", "Tommen Baratheon", "Myrcella Baratheon"], Dothrakis: ["Khal Drogo"], Freys: ["Walder Frey"], Greyjoys: ["Balon Greyjoy", "Theon Greyjoy", "Yara Greyjoy"], Lannisters: ["Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Cersei Baratheon"], Redwyne: ["Olenna Tyrell"], Starks: ["Eddard Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon Stark", "Rickon Stark", "Jon Snow"], Targaryens: ["Daenerys Targaryen", "Viserys Targaryen"], Tullys: ["Catelyn Stark", "Lysa Arryn", "Edmure Tully", "Brynden Tully"], Tyrells: ["Margaery Baratheon", "Loras Tyrell"]}
+
